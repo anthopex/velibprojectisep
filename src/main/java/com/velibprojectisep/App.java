8b0df774cda16c7e9742d8a1dc7c7ff6bcb198b6
Victor Ely <vely@juniorisep.com>
2017-11-06 16:12:29 +0100

    Add Editorconfig

--- src/main/java/com/velibprojectisep/App.java
+++ src/main/java/com/velibprojectisep/App.java
@@ -4,7 +4,10 @@ package com.velibprojectisep;
  * Hello world!
  *
  */
-public class App 
+
+/* "minLng":2.222189235680779,"minLat":48.806380010297374,"maxLng":2.479464268709081,"maxLat":48.91519864243704,"tstp":1364307075371 */
+
+public class App
 {
     public static void main( String[] args )
     {
